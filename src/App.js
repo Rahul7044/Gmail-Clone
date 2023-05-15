2e6262d79bcc6e3d555c8145caa6391420e0b6b8
Rahul7044 <rahulkumar581999@gmail.com>
2023-05-15 21:54:48 +0530

    read-messages-properly

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import LoginSignup from "./components/LoginSignup/LoginSignup";
 import { Route, Routes } from "react-router-dom";
 import ComposeEmail from "./components/Pages/Home/ComposeEmail";
 import Header from "./components/Pages/Home/Header";
+import InboxPage from "./components/Pages/Inbox/InboxPage";
+import ViewMessage from "./components/Pages/Inbox/ViewMessagePage";
+
 
 function App() {
   const isLogin = useSelector(state=> state.auth.userAuth.isLogin);
   console.log(isLogin)
-
   return (
     <>
     <Header></Header>
@@ -16,6 +18,9 @@ function App() {
       {isLogin && <Route path={'/compose-email'} element={<ComposeEmail/>}></Route>}
       {!isLogin && <Route path={'*'} element={<LoginSignup/>}></Route>}
       {isLogin && <Route path={'*'} element={<ComposeEmail />}></Route>}
+      {/* <Route path="inbox" element={<InboxPage/>}></Route> */} {/* this line must be added in the previouse commit*/} 
+      {isLogin&& <Route path="inbox" element={<InboxPage/>}></Route> }
+      <Route path="/inbox/:id" element={<ViewMessage/>}></Route>
 
     </Routes>
 
