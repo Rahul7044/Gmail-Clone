4daa14f066dd7ed31e8a793b01ffc34d4cf8c2d5
Rahul7044 <rahulkumar581999@gmail.com>
2023-05-16 11:26:54 +0530

    Delete Mail

--- src/components/Pages/Inbox/EmailItem.js
+++ src/components/Pages/Inbox/EmailItem.js
@@ -2,32 +2,30 @@ import React from 'react';
 import { ListGroup, Button } from 'react-bootstrap';
 import BlueDot from './BlueDot';
 import { useNavigate } from 'react-router-dom';
-
-function EmailItem({ senderEmail, subject, onDelete, unRead, id, content }) {
-    const navTo = useNavigate();
-
-    const readMessageHandler = () => {
-        navTo(`/inbox/${id}`)
-    }
-
-
-    return (
-        <ListGroup.Item style={{ backgroundColor: '#f8f9fa', }} onClick={readMessageHandler}>
-
-            <div className="d-flex align-items-center">
-                {unRead && <BlueDot></BlueDot>}
-                <div style={{ float: 'right', width: '90%', marginLeft: '20px' }}>
-                    <div>{` Received From: ${senderEmail}`}</div>
-                    <div>{subject}</div>
-                </div>
-                <div style={{ float: 'right', width: '90%', textAlign: 'right' }}>
-                    <Button variant="danger" onClick={onDelete}>
-                        Delete
-                    </Button>
-                </div>
-            </div>
-        </ListGroup.Item>
-    );
+function EmailItem({ senderEmail, subject, onDelete , unRead , id , content }) {
+const navTo = useNavigate();
+const readMessageHandler = ()=>{
+      navTo(`/inbox/${id}`)
 }
 
+
+  return (
+    <ListGroup.Item style={{ backgroundColor: '#f8f9fa'}} className="d-flex align-items-center" >
+
+      <div className="d-flex align-items-center" onClick={readMessageHandler} style={{float:'right', width:'90%', marginLeft:'20px' }}>
+        {unRead && <BlueDot></BlueDot> }
+        <div style={{marginLeft:'20px' }}>
+          <div>{` Received From: ${senderEmail}`}</div>
+          <div>{subject}</div>
+        </div>
+
+      </div>
+      <div style={{ textAlign:'right'}}>
+      <Button variant="danger" onClick={()=>onDelete(id)} style={{float:'right'}}>
+        Delete
+      </Button>
+    </div>
+    </ListGroup.Item>
+  );
+}
 export default EmailItem;
--- src/components/Pages/Inbox/InboxPage.js
+++ src/components/Pages/Inbox/InboxPage.js
@@ -3,61 +3,82 @@ import EmailItem from "./EmailItem";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useState } from "react";
-
-async function getMails(email) {
-    let emailID = email;
+async function getMails(email){
+    let emailID = email ;
     emailID = emailID.replace(/[.@]/g, "")
     try {
-        const response = await fetch(`https://mail-box-client-65adf-default-rtdb.firebaseio.com/mailbox/%20%20%20%20%20%20users/${emailID}/recivedmails.json`)
-        const data = await response.json();
-        console.log(email)
-        console.log(response)
-        if (!response.ok) {
-            throw new Error(data.error)
-        }
-        return data;
+      const response =  await fetch(`https://user-login-signup-330a7-default-rtdb.firebaseio.com/mailbox/%20%20%20%20%20%20users/${emailID}/recivedmails.json`)
+      const data = await response.json();
+      console.log(email)
+      console.log(response)
+      if(!response.ok){
+        throw new Error(data.error)
+      }
+      return data ;
     } catch (error) {
-        alert(error)
+      alert(error)
     }
 
-}
+  }
 
-const InboxPage = () => {
-    const email = useSelector(state => state.auth.userAuth.email)
-    const [recievedMailsList, setRecievedMailsList] = useState({});
-
-
-    useEffect(() => {
-        getMails(email).then(data => {
-            setRecievedMailsList(data);
-        });
-    }, [email])
-    const Emails = [];
-    for (let key in recievedMailsList) {
-        const id = key;
-        const subject = recievedMailsList[key].subject;
-        const reciveFrom = recievedMailsList[key].reciveFrom;
-        const unRead = recievedMailsList[key].unRead;
-        const content = recievedMailsList[key].content;
-        Emails.push(
-            <EmailItem key={id} id={id} senderEmail={reciveFrom} subject={subject} unRead={unRead} content={content} ></EmailItem>
-        )
+async function deleteMail(email , id){
+  let emailID = email ;
+  emailID = emailID.replace(/[.@]/g, "")
+  try {
+    const response =  await fetch(`https://new-one-6f2ca-default-rtdb.firebaseio.com/mailbox/%20%20%20%20%20%20users/${emailID}/recivedmails/${id}.json`,
+    {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    if(!response.ok){
+      throw new Error('unable to delete')
     }
+
+  } catch (error) {
+    alert(error)
+  }
+}  
+
+const InboxPage = ()=>{
+  const email = useSelector(state=>state.auth.userAuth.email)
+  const [recievedMailsList , setRecievedMailsList] = useState({});
+  const deleteMailHandler = (id)=>{
+     deleteMail(email ,id);
+    delete recievedMailsList[id] ;
+    setRecievedMailsList({...recievedMailsList})
+
+  }
+
+  useEffect(()=>{
+    getMails(email).then(data=>{
+       setRecievedMailsList(data);
+    });
+  },[email])
+  const Emails = [];
+  for (let key in recievedMailsList){
+    const id = key ;
+    const subject = recievedMailsList[key].subject ;
+    const reciveFrom = recievedMailsList[key].reciveFrom ;
+    const unRead = recievedMailsList[key].unRead ;
+    const content = recievedMailsList[key].content;
+    Emails.push(
+        <EmailItem key={id} id ={id} senderEmail={reciveFrom} subject={subject} unRead={unRead} content={content} onDelete={deleteMailHandler} ></EmailItem>
+    )
+  }
 
     return (
         <>
-            <Container>
-                <Card style={{ padding: '40px', margin: '40px' }}>
-                    <Card.Title> My Inbox</Card.Title>
-                    <Card.Body>
-                        <ListGroup>
-                            {Emails}
-                        </ListGroup>
-                    </Card.Body>
-                </Card>
-            </Container>
+        <Container>
+        <Card style={{padding:'40px' , margin:'40px'}}>
+        <Card.Title> My Inbox</Card.Title>    
+        <Card.Body> 
+          <ListGroup>
+            {Emails.reverse()} 
+          </ListGroup>
+        </Card.Body>  
+        </Card>
+        </Container>
         </>
     )
 }
-
-export default InboxPage;
+export default InboxPage ;
